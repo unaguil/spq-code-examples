97529ccd78a41a5d83e09bf16a9777c83967a1ca
Unai Aguilera <unai.aguilera@deusto.es>
2022-05-25 09:35:41 +0200

    I18n example with dependency jars and application exec from command lin

---
+++ maven-examples/example-jdo-rdbms-mysql-resourcebundle/README.md
@@ -0,0 +1,43 @@
+Datanucleus example with Maven and I18N
+=======================================
+
+This example relies on the DataNucleus Maven plugin. Check the database configuration in the *datanucleus.properties* file and the JDBC driver dependency specified in the *pom.xml* file.
+
+Run the following command to build everything and enhancing the DB classes:
+
+   mvn clean compile
+
+Make sure that the database was correctly configured. Use the contents of the file *create-productsdb.sql* to create the database and grant privileges. For example,
+
+   mysql –uroot < create-productsdb.sql
+
+Run the following command to create database schema for this sample.
+
+   mvn datanucleus:schema-create
+
+Run the following command to launche the main example class 
+   
+   mvn exec:java
+
+Run the following command to remove the database schema
+   
+   mvn datanucleus:schema-delete
+
+Run the following command to generate the Javadoc documentation inside the *target/site/apidocs* directory
+
+   mvn javadoc:javadoc
+
+Finally, to generate the distributable application with its dependencies run the command
+
+   mvn package
+
+that will build the application jar and copy all the required libraries to the *target/jar*.
+Finally, run the following command to execute the application from the command line
+
+(Windows classpath separator ;)
+
+   java -cp "target/jars/*";target/Datanucleus-Samples-JDO-MySQL-I18N-3.0.jar org.datanucleus.samples.jdo.tutorial.Main
+
+(Linux classpath separator :)
+
+   java -cp "target/jars/*":target/Datanucleus-Samples-JDO-MySQL-I18N-3.0.jar org.datanucleus.samples.jdo.tutorial.Main
---
+++ maven-examples/example-jdo-rdbms-mysql-resourcebundle/src/main/java/org/datanucleus/samples/jdo/tutorial/Main.java
@@ -0,0 +1,181 @@
+/**********************************************************************
+Copyright (c) 2003 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.samples.jdo.tutorial;
+
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Extent;
+import javax.jdo.Query;
+import javax.jdo.JDOHelper;
+import javax.jdo.Transaction;
+
+// To support resource bundle
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
+/**
+ * Controlling application for the DataNucleus Tutorial using JDO.
+ * Relies on the user defining a file "datanucleus.properties" to be in the CLASSPATH
+ * and to include the JDO properties for the DataNucleus PersistenceManager.
+ * @author      Diego Lopez-de-Ipina 
+ * @version     1.0                                    
+ * @since       2012-02-01        
+ */
+public class Main
+{
+	private static final Logger logger = Logger.getLogger("Main");
+    @SuppressWarnings("unchecked")
+	public static void main(String args[])
+    {
+		// ResourceBundle class will use SystemMessages.properties file
+		ResourceBundle resourceBundle = ResourceBundle.getBundle("SystemMessages", Locale.getDefault());
+		resourceBundle = ResourceBundle.getBundle("SystemMessages",	Locale.forLanguageTag("en"));
+
+		logger.info(resourceBundle.getString("starting_msg"));
+			
+        // Create a PersistenceManagerFactory for this datastore
+        PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+
+		logger.info(resourceBundle.getString("app_title"));
+        logger.info(resourceBundle.getString("app_underline"));
+
+        // Persistence of a Product and a Book.
+        PersistenceManager pm = pmf.getPersistenceManager();
+        Transaction tx=pm.currentTransaction();
+        try
+        {
+            tx.begin();
+			logger.info(resourceBundle.getString("persisting_title_msg"));
+            Product product = new Product("Sony Discman","A standard discman from Sony",200.00);
+            Book book = new Book("Lord of the Rings by Tolkien","The classic story",49.99,"JRR Tolkien", "12345678", "MyBooks Factory");
+            pm.makePersistent(product);
+            pm.makePersistent(book);
+ 
+            tx.commit();
+			logger.info(resourceBundle.getString("persisting_body_msg"));
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+        }
+
+        // Basic Extent of all Products
+        pm = pmf.getPersistenceManager();
+        tx = pm.currentTransaction();
+		// changing to Spanish now
+		resourceBundle = ResourceBundle.getBundle("SystemMessages",	Locale.forLanguageTag("es"));
+        try
+        {
+            tx.begin();
+			logger.info(resourceBundle.getString("retrieving_products_msg"));
+            Extent<Product> e = pm.getExtent(Product.class, true);
+            Iterator<Product> iter = e.iterator();
+            while (iter.hasNext())
+            {
+                Object obj = iter.next();
+                logger.info(">  " + obj);
+            }
+            tx.commit();
+        }
+        catch (Exception e)
+        {
+			logger.info(resourceBundle.getString("retrieving_exception_msg") + e.getMessage());
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+        }
+        System.out.println("");
+
+        // Perform some query operations
+        pm = pmf.getPersistenceManager();
+        tx = pm.currentTransaction();
+        try
+        {
+            tx.begin();
+			logger.info(resourceBundle.getString("executing_query_msg"));
+            Extent<Product> e=pm.getExtent(Product.class,true);
+            Query<Product> q=pm.newQuery(e, "price < 150.00");
+			q.setOrdering("price ascending");                        
+            for (Product product : (List<Product>)q.execute()) {
+            	logger.info(">  " + product);
+            	if (product instanceof Book)
+                {
+                    Book b = (Book)product;
+                    // Give an example of an update
+                    b.setDescription(resourceBundle.getString("price_reduced_msg"));
+                    logger.info(resourceBundle.getString("price_reduced_msg"));
+                }
+    
+            }
+
+            tx.commit();
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+        }
+        
+		// changing to Basque now
+		resourceBundle = ResourceBundle.getBundle("SystemMessages",	Locale.forLanguageTag("eu"));
+		
+		
+        // Clean out the database
+        pm = pmf.getPersistenceManager();
+        tx = pm.currentTransaction();
+        try
+        {
+            tx.begin();
+            logger.info(resourceBundle.getString("deleting_products_msg"));
+            Query<Product> q = pm.newQuery(Product.class);
+            long numberInstancesDeleted = q.deletePersistentAll();
+            logger.info(resourceBundle.getString("deleted_msg") + numberInstancesDeleted + resourceBundle.getString("products_msg"));
+
+            tx.commit();
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+        }
+
+        logger.info("");
+        logger.info(resourceBundle.getString("end_tutorial_msg"));
+		pmf.close();
+    }
+}
